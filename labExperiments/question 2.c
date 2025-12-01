66a2ead95b8926bb395ba5a613ac5200661b0fc9
Dhruv <dhruvaggarwal1857@gmail.com>
2025-12-01 13:43:02 +0530

    Delete labExperiments/question 2.c

--- labExperiments/question 2.c
+++
@@ -1,9 +0,0 @@
-//2.Write a C Program to print the address in multiple lines (new line).
-#include <stdio.h>
-int main()
-{
-    printf("I live in Sri Ganganagar,\n");
-    printf("Rajasthan,\n");
-    printf("India");
-    return 0;
-}
